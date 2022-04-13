0ff032e242389cfefc8bb3f931fb2494dea09082
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-04-13 10:38:27 +0900

    📑 :: Controller에서 사용할 DTO 클래스 생성

--- spring-webservice/src/main/java/com/pyaq0/springwebservice/dto/posts/PostsSaveRequestDto.java
+++ spring-webservice/src/main/java/com/pyaq0/springwebservice/dto/posts/PostsSaveRequestDto.java
@@ -1,2 +1,23 @@
-package com.pyaq0.springwebservice.dto.posts;public class PostsSaveRequestDto {
+package com.pyaq0.springwebservice.dto.posts;
+
+import com.pyaq0.springwebservice.domain.posts.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
 }
