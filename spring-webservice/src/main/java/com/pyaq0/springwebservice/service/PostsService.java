b316d5ffebb2c1c9ee0cd96e8c00cd6ff2796d2c
soooomm00@gmail.com <soooomm00@gmail.com>
2022-04-14 13:13:17 +0900

    📝 :: 옵션(readOnly = true) 추가

--- spring-webservice/src/main/java/com/pyaq0/springwebservice/service/PostsService.java
+++ spring-webservice/src/main/java/com/pyaq0/springwebservice/service/PostsService.java
@@ -1,19 +1,24 @@
 package com.pyaq0.springwebservice.service;
 
 import com.pyaq0.springwebservice.domain.posts.PostsRepository;
+import com.pyaq0.springwebservice.dto.posts.PostsMainResponseDto;
 import com.pyaq0.springwebservice.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class PostsService {
     private PostsRepository postsRepository;
 
-    @Transactional
-    public Long save(PostsSaveRequestDto dto){
-        return postsRepository.save(dto.toEntity()).getId();
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
