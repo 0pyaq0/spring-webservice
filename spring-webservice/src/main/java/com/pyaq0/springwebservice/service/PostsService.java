e6e959ec7feb70333eb9a8d499fb4a94856f0f04
soooomm00@gmail.com <soooomm00@gmail.com>
2022-04-14 11:10:47 +0900

    📑 :: service 메소드 구현 (PostsService 클래스 생성)

---
+++ spring-webservice/src/main/java/com/pyaq0/springwebservice/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.pyaq0.springwebservice.service;
+
+import com.pyaq0.springwebservice.domain.posts.PostsRepository;
+import com.pyaq0.springwebservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto){
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+}
