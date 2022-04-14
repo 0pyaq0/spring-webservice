0e6b7eeb959550045703d2154f9d4d57673ea5e6
soooomm00@gmail.com <soooomm00@gmail.com>
2022-04-14 11:14:16 +0900

    📝 :: WebControllerTest 에러 (수정 필요) - #6

--- spring-webservice/src/main/java/com/pyaq0/springwebservice/web/webController.java
+++ spring-webservice/src/main/java/com/pyaq0/springwebservice/web/webController.java
@@ -3,15 +3,13 @@ package com.pyaq0.springwebservice.web;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 
 @Controller
 @AllArgsConstructor
-public class webController {
+public class WebController {
 
     @GetMapping("/")
     public String main() {
         return "main";
     }
-
 }
